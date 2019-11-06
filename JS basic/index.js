ce71446b5cfd0020900fede2e60335688ae0ef67
abhishekharshvardhan <abhishekharshvardhan@gmail.com>
2019-11-06 09:30:04 +0530

    js basics

--- JS basic/index.js
+++ JS basic/index.js
@@ -1,1 +1,39 @@
-console.log('hi there');
+//extensions
+// 1. prettier
+// 2. live server
+// 3. eslint
+// 4. bracket pair colorizer
+
+// Primitive
+// 1. Number
+// 2. boolean
+// 3. string
+// 4. null
+// 5. undefined
+
+//reference types
+// 1. Arrays
+// 2. Objects
+
+// functions
+
+// 1. using function keyword
+// function greet(name) {
+// 	return 'Abhishek'
+// }
+
+//  2. Arrow function
+// const greet = () => 'Abhishek';
+
+// 3. Objects
+const blog = {
+	title: 'My first blog',
+	description: 'Some desc',
+	authors: ['Abhishek', 'rejoy'],
+	isPublished: true
+};
+
+const blog2 = new Object(blog);
+// blog.getBolgTitle.bind(blog2);
+
+console.log(blog2.getBolgTitle.call(blog2));
---
+++ JS basic/log.js
@@ -0,0 +1,3 @@
+// const blog = require('./index');
+
+console.log(blog);
