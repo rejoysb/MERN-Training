a9f6b6591fc37df8e196486caf93fe9f68feaac2
abhishekharshvardhan <abhishekharshvardhan@gmail.com>
2019-11-13 08:23:19 +0530

    maxchar tips

--- JS basic/problems/maxChar.js
+++ JS basic/problems/maxChar.js
@@ -5,4 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+// 1. Map/HashMap, 2. Stack
+
+// {
+//   a:1,
+//   b:1,
+//   c:5,
+//   d:1
+// }
+
+// result ={}
+// result[var]
+
+// for in  -> key -> result[var]
+
 function maxChar(str) {}
